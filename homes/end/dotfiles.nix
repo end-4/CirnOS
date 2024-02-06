a74022374bb221fd9da74bb5ae3c5ea70819db41
end-4 <97237370+end-4@users.noreply.github.com>
2024-02-06 22:52:39 +0700

    sync

--- homes/end/dotfiles.nix
+++ homes/end/dotfiles.nix
@@ -1,7 +1,7 @@
 { config, inputs, pkgs, ... }: let 
   symlink = config.lib.file.mkOutOfStoreSymlink;
 in {
-  # home.file = {
-  #   ".config/starship.toml".source = symlink ./.config/starship.toml;
-  # };
+  home.file = {
+    ".config/starship.toml".source = symlink ./.config/starship.toml;
+  };
 }
