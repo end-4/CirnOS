73025792cbfd00332d00837fad17a5df993ab757
end-4 <97237370+end-4@users.noreply.github.com>
2024-02-07 07:08:05 +0700

    sync

--- hosts/CirnOS/laptop.nix
+++ hosts/CirnOS/laptop.nix
@@ -7,31 +7,31 @@
   # };
 
   # nvidia
-  # hardware.opengl = {
-  #   enable = true;
-  #   driSupport = true;
-  #   driSupport32Bit = true;
-  # };
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
 
-  # services.xserver.videoDrivers = ["nvidia"];
+  services.xserver.videoDrivers = [ "nvidia" ];
 
-  # hardware.nvidia = {
-  #   prime = {
-  #     offload.enable = true;
-  #     offload.enableOffloadCmd = true;
-  #     nvidiaBusId = "PCI:1:0:0";
-  #     amdgpuBusId = "PCI:6:0:0";
-  #   };
+  hardware.nvidia = {
+    prime = {
+      offload.enable = true;
+      offload.enableOffloadCmd = true;
+      nvidiaBusId = "PCI:1:0:0";
+      amdgpuBusId = "PCI:6:0:0";
+    };
 
-  #   modesetting.enable = true;
+    modesetting.enable = true;
 
-  #   powerManagement = {
-  #     enable = true;
-  #     finegrained = true;
-  #   };
+    powerManagement = {
+      enable = true;
+      finegrained = true;
+    };
 
-  #   open = true;
-  #   nvidiaSettings = false; # gui app
-  #   package = config.boot.kernelPackages.nvidiaPackages.latest;
-  # };
+    open = true;
+    nvidiaSettings = false; # gui app
+    package = config.boot.kernelPackages.nvidiaPackages.latest;
+  };
 }
