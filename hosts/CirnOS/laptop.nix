7797b7e89a3543435c67dd98e4aea060e2112c4e
end-4 <97237370+end-4@users.noreply.github.com>
2024-02-07 19:10:10 +0700

    sync

--- hosts/CirnOS/laptop.nix
+++ hosts/CirnOS/laptop.nix
@@ -1,10 +1,23 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 {
-  # asusctl
-  # services.asusd = {
-  #   enable = true;
-  #   enableUserService = true;
-  # };
+  systemd.services.battery-charge-threshold =
+    let
+      CHARGE_THRESHOLD = "80";
+    in
+    {
+      enable = true;
+      description = "Set the battery charge threshold";
+      startLimitBurst = 0;
+      after = [ "multi-user.target" ];
+      serviceConfig = {
+        Type = "oneshot";
+        Restart = "on-failure";
+        ExecStart = "${pkgs.bash}/bin/bash -c '${pkgs.coreutils}/bin/echo ${CHARGE_THRESHOLD} > /sys/class/power_supply/BATT/charge_control_end_threshold'";
+        # ...
+      };
+      wantedBy = [ "multi-user.target" ];
+      # ...
+    };
 
   # nvidia
   hardware.opengl = {
