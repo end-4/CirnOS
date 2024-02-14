54207264f031fca379af754d03215c7fea7852bb
end-4 <97237370+end-4@users.noreply.github.com>
2024-02-14 21:07:55 +0700

    ags: add quick scripts

--- homes/end/.config/ags/widgets/sideleft/module.js
+++ homes/end/.config/ags/widgets/sideleft/module.js
@@ -1,32 +1,46 @@
 import Widget from 'resource:///com/github/Aylur/ags/widget.js';
+import { setupCursorHover } from '../../lib/cursorhover.js';
+import { MaterialIcon } from '../../lib/materialicon.js';
 const { Box, Button, Icon, Label, Revealer } = Widget;
 
 export const SidebarModule = ({
+    icon,
     name,
     child
 }) => {
-    const header = Button({
-        child: Box({
-            children: [
-                Label({
-                    className: 'txt-norm txt',
-                    label: `${name}`,
-                }),
-                Box({
-                    hexpand: true,
-                }),
-                Label({
-                    className: 'sidebar-module-btn-arrow',
-                })
-            ]
-        }),
-        onClicked: () => content.revealChild = !content.revealChild,
+    const headerButtonIcon = MaterialIcon('expand_more', 'norm');
+    const header = Box({
+        className: 'spacing-h-10',
+        children: [
+            icon,
+            Label({
+                className: 'txt-norm txt',
+                label: `${name}`,
+            }),
+            Box({
+                hexpand: true,
+            }),
+            Button({
+                className: 'sidebar-module-btn-arrow txt',
+                child: headerButtonIcon,
+                onClicked: () => {
+                    console.log('clicked');
+                    content.revealChild = !content.revealChild;
+                    headerButtonIcon.label = content.revealChild ? 'expand_less' : 'expand_more';
+                },
+                setup: setupCursorHover,
+            })
+        ]
     });
     const content = Revealer({
         revealChild: true,
         transition: 'slide_down',
         transitionDuration: 200,
-        child: child,
+        child: Box({
+            className: 'margin-top-5',
+            homogeneous: true,
+            children: [child],
+        }),
     });
     return Box({
         className: 'sidebar-module',
