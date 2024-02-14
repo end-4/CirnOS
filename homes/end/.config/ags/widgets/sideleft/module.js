4108c1a68660fea50748105e7dd6c2be67f399b0
end-4 <97237370+end-4@users.noreply.github.com>
2024-02-14 13:20:51 +0700

    ags: no more condition race for icons

--- homes/end/.config/ags/widgets/sideleft/module.js
+++ homes/end/.config/ags/widgets/sideleft/module.js
@@ -1,30 +1,39 @@
 import Widget from 'resource:///com/github/Aylur/ags/widget.js';
-const { Box, Button, Label } = Widget;
+const { Box, Button, Icon, Label, Revealer } = Widget;
 
 export const SidebarModule = ({
     name,
     child
 }) => {
+    const header = Button({
+        child: Box({
+            children: [
+                Label({
+                    className: 'txt-norm txt',
+                    label: `${name}`,
+                }),
+                Box({
+                    hexpand: true,
+                }),
+                Label({
+                    className: 'sidebar-module-btn-arrow',
+                })
+            ]
+        }),
+        onClicked: () => content.revealChild = !content.revealChild,
+    });
+    const content = Revealer({
+        revealChild: true,
+        transition: 'slide_down',
+        transitionDuration: 200,
+        child: child,
+    });
     return Box({
         className: 'sidebar-module',
         vertical: true,
         children: [
-            Button({
-                child: Box({
-                    children: [
-                        Label({
-                            className: 'txt-small txt',
-                            label: `${name}`,
-                        }),
-                        Box({
-                            hexpand: true,
-                        }),
-                        Label({
-                            className: 'sidebar-module-btn-arrow',
-                        })
-                    ]
-                })
-            })
+            header,
+            content,
         ]
     });
 }
--- homes/end/.config/ags/widgets/sideleft/quickscripts.js
+++ homes/end/.config/ags/widgets/sideleft/quickscripts.js
@@ -1,11 +1,54 @@
+const { Gtk } = imports.gi;
+import App from 'resource:///com/github/Aylur/ags/app.js';
 import Widget from 'resource:///com/github/Aylur/ags/widget.js';
 import * as Utils from 'resource:///com/github/Aylur/ags/utils.js';
 const { execAsync, exec } = Utils;
-const { Box, Button, EventBox, Label, Scrollable } = Widget;
+const { Box, Button, EventBox, Icon, Label, Scrollable } = Widget;
 import { SidebarModule } from './module.js';
 
+Gtk.IconTheme.get_default().append_search_path(`${App.configDir}/assets`);
+const distroName = exec(`bash -c 'cat /etc/os-release | grep "^NAME=" | cut -d "=" -f 2'`).trim();
+
+const scripts = [
+    {
+        icon: 'nixos-symbolic',
+        name: 'Trim system generations to 5',
+        command: `sudo ${App.configDir}/scripts/quickscripts/nixos-trim-generations.sh 5 0 system`,
+        enabled: distroName == 'NixOS',
+    },
+    {
+        icon: 'nixos-symbolic',
+        name: 'Trim home manager generations to 5',
+        command: `${App.configDir}/scripts/quickscripts/nixos-trim-generations.sh 5 0 home-manager`,
+        enabled: distroName == 'NixOS',
+    },
+];
+
 export const QuickScripts = () => SidebarModule({
+    icon: 'nixos-symbolic',
     name: 'Quick scripts',
     child: Box({
+        vertical: true,
+        className: 'spacing-v-5',
+        children: scripts.map((script) => script.enabled ? Button({
+            child: Box({
+                className: 'spacing-h-5',
+                children: [
+                    Icon({
+                        className: 'sidebar-module-btn-icon txt txt-large',
+                        icon: script.icon,
+                    }),
+                    Label({
+                        className: 'txt txt-small',
+                        hpack: 'start',
+                        label: script.name,
+                        tooltipText: script.command,
+                    }),
+                ],
+            }),
+            onClicked: () => {
+                execAsync([`bash`, `-c`, `foot fish -C "${script.command}"`, `&`]).catch(print);
+            }
+        }) : null)
     })
-})
+});
